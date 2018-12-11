6df19a53dfb2554fc90fe5a7305802fea3f0cd2a
atomcorp <thomasmaxwellsmith@gmail.com>
2018-12-11 22:33:44 +0000

    manual using tiles

--- src/components/Tile/TileExamples.js
+++ src/components/Tile/TileExamples.js
@@ -1,15 +1,35 @@
 import React from 'react';
-
+import css from './TileExamples.module.css';
 import Tile from './Tile';
 
 const TileExamples = () => (
   <section>
-    <Tile index={0} />
-    <Tile index={1} />
-    <Tile index={2} />
-    <Tile index={3} />
-    <Tile index={4} />
-    <Tile index={5} />
+    <section className={css.row}>
+      <Tile index={0} />
+      <Tile index={1} />
+      <Tile index={2} />
+      <Tile index={3} />
+      <Tile index={4} />
+      <Tile index={5} />
+    </section>
+    <section className={css.grid}>
+      <Tile index={2} />
+      <Tile index={3} />
+      <Tile index={0} />
+      <Tile index={6} />
+      <Tile index={6} />
+      <Tile index={0} />
+      <Tile index={4} />
+      <Tile index={1} />
+      <Tile index={1} />
+      <Tile index={4} />
+      <Tile index={6} />
+      <Tile index={0} />
+      <Tile index={0} />
+      <Tile index={6} />
+      <Tile index={3} />
+      <Tile index={2} />
+    </section>
   </section>
 );
 
