2f7f8242700437a682de1497f86e9a5691ff811a
atomcorp <thomasmaxwellsmith@gmail.com>
2018-12-12 22:30:13 +0000

    fixed not printing NW arrow

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -2,29 +2,47 @@ import React from 'react';
 import css from './Board.module.css';
 import Tile from '../Tile/Tile';
 
+/**
+ * is greater or equal to min
+ * is less than, but not equal, to max
+ * @param {number} min
+ * @param {number} max
+ * @return {number} integer
+ */
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+/**
+ *
+ * @param {string} type
+ * @return {number} integer
+ */
 const tilesSwitch = (type) => {
   switch (type) {
     case 'arrows':
-      return getRandomInt(1, 4);
-    case 'noFills':
-      return getRandomInt(0, 4);
-    case 'noEmpties':
       return getRandomInt(1, 5);
+    case 'noFills':
+      return getRandomInt(0, 5);
+    case 'noEmpties':
+      return getRandomInt(1, 6);
     case 'all':
     default:
-      return getRandomInt(0, 5);
+      return getRandomInt(0, 6);
   }
 };
 
+/**
+ *
+ * @param {number} x
+ * @param {number} y
+ */
 const return2dArray = (x, y) => {
   return Array.from(Array(y)).fill(Array.from(Array(x)).fill(0));
 };
 
 let root = document.documentElement;
+
 const Board = (props) => {
   root.style.setProperty('--wide', props.wide);
   root.style.setProperty('--tall', props.tall);
